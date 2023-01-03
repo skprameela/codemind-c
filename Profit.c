61437a638e0d9651c6df2c2725008552857fdf32
skprameela <119730166+skprameela@users.noreply.github.com>
2023-01-03 23:05:00 +0530

    #include<stdio.h> int main() {     int cp,sp,p,l;     scanf("%d%d",&cp,&sp);     p=sp-cp;     l=cp-sp;     if(sp>cp)     {         printf("Profit");     }     else     {         printf("Loss");
        }
    }

---
+++ Profit.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+    int cp,sp,p,l;
+    scanf("%d%d",&cp,&sp);
+    p=sp-cp;
+    l=cp-sp;
+    if(sp>cp)
+    {
+        printf("Profit");
+    }
+    else
+    {
+        printf("Loss");
+        
+    }
+}
